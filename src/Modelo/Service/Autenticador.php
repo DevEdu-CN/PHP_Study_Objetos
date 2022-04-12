769ccf534bdaaf19f1b3a1339a0497ddd5b46976
Eduardo <luiz.ed2013@gmail.com>
2022-04-11 23:09:40 -0300

    Banco Final

---
+++ src/Modelo/Service/Autenticador.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Alura\Banco\Service;
+
+
+class Autenticador
+{
+    public function tentaLogin(Autenticavel $autenticavel, string $senha): void
+    {
+        if ($autenticavel->podeAutenticar($senha)) {
+            echo "Ok. Usuário logado no sistema";
+        } else {
+            echo "Ops. Senha incorreta.";
+        }
+    }
+}
+?>
